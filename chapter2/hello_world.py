3c72ba27b80bf24b2878f7e7e60f3e95c810a9a3
NullEvasion <vovan4uk161rus@mail.ru>
2025-12-28 07:45:16 +0300

    Done task 2.1 and 2.2

---
+++ chapter2/hello_world.py
@@ -0,0 +1,12 @@
+print("Hello Python world!")
+
+message="Hello Python world!"
+print(message)
+message="Hello Python Crash Course world!"
+print(message)
+
+message="Hello Python Crash Course reader!"
+#print(mesage)
+
+mesage="Hello Python Crash Course reader!"
+print(mesage)
