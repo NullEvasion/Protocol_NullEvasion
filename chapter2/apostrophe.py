dea6ac08ff7ed9caca92979ec0b8a3d20b06c465
NullEvasion <vovan4uk161rus@mail.ru>
2026-01-02 06:10:11 +0300

    Work with apostrophes

--- chapter2/apostrophe.py
+++ chapter2/apostrophe.py
@@ -1,3 +1,10 @@
+'''
+Author       : NullEvasion
+Date         : 29.12.2025
+'''
+
+
+
 message="One of Python's strengths is its diverse community."
 print(message)
 #message='One of Python's strengths is its diverse community.'
