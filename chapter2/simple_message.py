14ccfd91c828c5a0604f1dbd2153d548d0083c21
NullEvasion <vovan4uk161rus@mail.ru>
2026-01-05 13:23:22 +0300

    Done task 2.1

--- chapter2/simple_message.py
+++ chapter2/simple_message.py
@@ -1,10 +1,12 @@
 '''
 Author       : NullEvasion
 Date         : 28.12.2025
+Last edit	 : 05.01.2026
 '''
 
 
 
 #2.1 Простое сообщение: сохраните сообщение в переменной и выведите его на экран.
+print(f"\n---2.1---")
 message="HyperX"
 print(message)
