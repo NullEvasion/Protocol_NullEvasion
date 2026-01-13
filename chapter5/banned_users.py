759121fde42c1bdc7ffcec8650851ee44afb68c4
NullEvasion <vovan4uk161rus@mail.ru>
2026-01-13 11:07:14 +0300

    feat: use 'not in' operator to check for banned users

---
+++ chapter5/banned_users.py
@@ -0,0 +1,14 @@
+'''
+Author       : NullEvasion
+Date         : 13.01.2026
+Last edit    : 13.01.2026
+'''
+
+
+
+
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.")
