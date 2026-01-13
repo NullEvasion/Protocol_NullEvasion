55c2de63952367c6c3690fa61568d1486712e585
NullEvasion <vovan4uk161rus@mail.ru>
2026-01-13 10:08:50 +0300

    Feat: implement the basic logic of the car brand and check the registers

---
+++ chapter5/cars.py
@@ -0,0 +1,27 @@
+'''
+Author       : NullEvasion
+Date         : 13.01.2026
+Last edit    : 13.01.2026
+'''
+
+
+
+
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())
+
+car='bmw'
+print(car == 'bmw')
+
+car='audi'
+print(car == 'bmw')
+
+car='Audi'
+print(car == 'audi')
+
+car='Audi'
+print(car.lower() == 'audi')
