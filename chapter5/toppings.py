2ffc9e8ed65c1a3e387028dbdacaf89d1adb4262
NullEvasion <vovan4uk161rus@mail.ru>
2026-01-13 10:23:37 +0300

    feat: add inequality and age checks

---
+++ chapter5/toppings.py
@@ -0,0 +1,15 @@
+'''
+Author       : NullEvasion
+Date         : 13.01.2026
+Last edit    : 13.01.2026
+'''
+
+
+
+
+requested_topping = 'mushrooms'
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+age = 18
+print(age == 18)
